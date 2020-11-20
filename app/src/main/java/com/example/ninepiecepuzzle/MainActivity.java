a6a852a4acd02073beec0b6abcd88dc6ed2dee00
Anton John Erik Sundström <sundstrom85@gmail.com>
2020-11-20 11:55:41 +0100

    Added listeners to MainActivity

--- app/src/main/java/com/example/ninepiecepuzzle/MainActivity.java
+++ app/src/main/java/com/example/ninepiecepuzzle/MainActivity.java
@@ -2,13 +2,62 @@ package com.example.ninepiecepuzzle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.util.EventLog;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text1, text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1 = findViewById(R.id.txt_Text1);
+        text2 = findViewById(R.id.txt_Text2);
+
+
+        text1.setOnLongClickListener(longClickListener);
+        //text2.setOnLongClickListener(longClickListener);
+
+        //text1.setOnDragListener(dragListener);
+        text2.setOnDragListener(dragListener);
+
     }
+
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData cd = ClipData.newPlainText("", "");
+            View.DragShadowBuilder dragShadow = new View.DragShadowBuilder(v);
+            v.startDrag(cd, dragShadow, v, 0);
+            return true;
+        }
+    };
+
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    final View fv = (View) event.getLocalState();
+
+                    if (fv.getId() == R.id.txt_Text1) {
+                        text2.setText("YAY");
+                    }
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    break;
+            }
+            return false;
+        }
+    };
 }
